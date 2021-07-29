1cd6b868297fe619e83ae1921c96509ad44b17b4
noyan <1.2.3.up.top.charm00ok001996991@gmail.com>
2021-07-30 00:33:29 +0900

    wip

--- src/Task.tsx
+++ src/Task.tsx
@@ -9,9 +9,11 @@ type Props = {
 };
 const Task = ({ task, index }: Props): JSX.Element => (
   <Draggable draggableId={task.id} index={index}>
-    {(provided) => (
+    {(provided, snapshot) => (
       <div
-        className="mb-2 p-2 border border-solid hover:border-gray-500 rounded transition duration-150 ease-in-out"
+        className={`mb-2 p-2 border border-solid  rounded transition duration-150 ease-in-out ${
+          snapshot.isDragging ? 'bg-green-100' : ''
+        }`}
         // eslint-disable-next-line @typescript-eslint/unbound-method
         ref={provided.innerRef}
         // eslint-disable-next-line react/jsx-props-no-spreading
